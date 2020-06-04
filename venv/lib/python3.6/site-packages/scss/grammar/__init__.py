7a67f715a6ac8ba2c3411f22ef7fb5381dd543de
Uzay-G <uzgirit@gmail.com>
2020-06-04 19:20:37 +0200

    fixed untracked files

--- venv/lib/python3.6/site-packages/scss/grammar/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Grammar and parser plumbing for Sass.  Much of this is generated or compiled
-in some fashion.
-"""
-from .scanner import NoMoreTokens
-from .scanner import Parser
-from .scanner import Scanner
-from .scanner import locate_blocks
-
-__all__ = ('NoMoreTokens', 'Parser', 'Scanner', 'locate_blocks')
