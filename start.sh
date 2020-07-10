231e9bd047c633cb8069d2fda280e4d1c0e7ad0e
Uzay-G <uzgirit@gmail.com>
2020-07-10 13:14:53 +0200

    background daemon that watches file changes and syncs to elasticsearch

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export $(cat .flaskenv | xargs)
+python3 check_changes.py &
 systemctl is-active --quiet elasticsearch && echo "elasticsearch is running" || (echo "Enter password to enable elasticsearch" && sudo service elasticsearch restart)
 FLASK_DEBUG=1 flask run
