2c1f92a91dbfccc804d2ba406e7e0506a8b177c3
Uzay-G <uzgirit@gmail.com>
2020-08-17 18:46:05 +0200

    fix errors

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ class Config(object):
     ELASTICSEARCH_ENABLED = int(os.environ.get('ELASTICSEARCH_ENABLED') or 0)
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     SECRET_KEY = os.urandom(32)
-    max_id = 0
