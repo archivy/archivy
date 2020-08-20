9beadc813b8ece7c668fd7dcc1be59ce50782043
Uzay-G <uzgirit@gmail.com>
2020-08-20 18:10:11 +0200

    reduce circular iports

--- main/__init__.py
+++ main/__init__.py
@@ -6,22 +6,21 @@ import requests
 from elasticsearch import Elasticsearch
 from flask import Flask
 from flask_scss import Scss
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
+from main import extensions
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-ELASTIC_SEARCH = None
-if app.config["ELASTICSEARCH_ENABLED"]:
-    ELASTIC_SEARCH = Elasticsearch([app.config["ELASTICSEARCH_URL"]])
+if extensions.ELASTICSEARCH:
     with open("elasticsearch.json", "r") as search_data:
         elastic_conf = json.load(search_data)
         # create index if not already existing
         try:
-            print(ELASTIC_SEARCH.indices.create(index=app.config["INDEX_NAME"], body=elastic_conf))
+            print(extensions.ELASTIC_SEARCH.indices.create(index=app.config["INDEX_NAME"], body=elastic_conf))
         except:
             print("Elasticsearch index already created")
 
@@ -29,9 +28,6 @@ if app.config["ELASTICSEARCH_ENABLED"]:
 app.jinja_options["extensions"].append("jinja2.ext.do")
 
 Scss(app)
-
-# small db to store pocket creds
-db = TinyDB("db.json")
 
 # create dir that will hold data if it doesn"t already exist
 DIRNAME = "data/"
@@ -42,7 +38,8 @@ from main import data
 from main import routes, models
 
 # get max id
-app.config["max_id"] = 0
+cur_id = extensions.get_max_id()
 for dataobj in data.get_items(structured=False):
-    app.config["max_id"] = max(app.config["max_id"], dataobj["id"])
-app.config["max_id"] += 1
+    cur_id = max(cur_id, dataobj["id"])
+
+extensions.set_max_id(cur_id)
