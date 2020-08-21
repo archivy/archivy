7cb65741b095379da8c1d0f4ad2e2ff6d39aaeac
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-21 21:02:16 +0300

    E302: expected 2 blank lines, found 1

--- archivy/extensions.py
+++ archivy/extensions.py
@@ -5,12 +5,14 @@ from archivy.config import Config
 
 DB = TinyDB(Config.APP_PATH + "/db.json")
 
+
 def get_max_id():
     max_id = DB.search(Query().name == "max_id")
     if not max_id:
         DB.insert({"name": "max_id", "val": 0})
         return 0
     return max_id[0]["val"]
+
 
 def set_max_id(val):
     DB.update(operations.set("val", val), Query().name == "max_id")
