24c778cd404ae2a62f979127c4705eaec936729b
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-21 20:20:00 +0300

    Save db.json and /data/ directory user level app directories
    to
    * XDG_DATA_DIR/archivy on Linux,
    * C:/Users/<username>/AppData/Local/archivy

--- archivy/config.py
+++ archivy/config.py
@@ -1,12 +1,14 @@
 import os
+import appdirs
 
 class Config(object):
     ELASTICSEARCH_ENABLED = os.environ.get("ELASTICSEARCH_ENABLED") or 0
     ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL") or "http://localhost:9200"
     SECRET_KEY = os.urandom(32)
     INDEX_NAME = "dataobj"
-    APP_PATH = os.path.dirname(os.path.abspath(__file__))
-
+    APP_PATH = os.getenv('ARCHIVY_DATA_DIR') or appdirs.user_data_dir('archivy')
+    if not os.path.exists(APP_PATH):
+        os.makedirs(APP_PATH)
     ELASTIC_CONF = {
         "settings": {
             "analysis": {
