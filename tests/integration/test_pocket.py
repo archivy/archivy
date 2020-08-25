07378e3a8a1897c49c5049338a4008fdf62da4ed
Clément Schreiner <clement@mux.me>
2020-08-26 00:47:21 +0200

    add test for pocket with empty title

--- tests/integration/test_pocket.py
+++ tests/integration/test_pocket.py
@@ -19,8 +19,32 @@ def test_parse_pocket(test_app, client, mocked_responses, pocket_fixture):
     """)
 
 
+
     r: Flask.response_class = client.get('/parse_pocket?new=1')
     assert r.status_code == 302
 
     dataobjs = data.get_items()
     assert len(dataobjs.child_files) == 1
+
+
+def test_pocket_with_empty_title(test_app, client, pocket_fixture,
+                                 mocked_responses):
+    """Test the /pocket endpoint
+
+    HTTP calls to the pocket API are mocked out
+    """
+
+    # fake website
+    mocked_responses.add(responses.GET, "https://example.com/", body="""<html>
+        <head></head><body><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </p></body></html>
+    """)
+
+    r: Flask.response_class = client.get('/parse_pocket?new=1')
+    assert r.status_code == 302
+
+    dataobjs = data.get_items()
+    assert len(dataobjs.child_files) == 1
+
+
