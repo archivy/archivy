f8a86d294635c3225af63e7c9ac11f5842348f99
Clément Schreiner <clement@mux.me>
2020-08-25 23:06:30 +0200

    add client fixture for testing routes

--- conftest.py
+++ conftest.py
@@ -1,6 +1,31 @@
-import pytest
+import os
+import shutil
+import tempfile
 
+import pytest
+import responses
+
+from archivy import app, config
 from archivy.models import DataObj
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    app_dir = tempfile.mkdtemp()
+    app.config['APP_PATH'] = app_dir
+    data_dir = os.path.join(app_dir, "data")
+    os.mkdir(data_dir)
+
+    with app.test_client() as client:
+        yield client
+
+    shutil.rmtree(app_dir)
+
+@pytest.fixture
+def mocked_responses():
+    with responses.RequestsMock() as rsps:
+        yield rsps
+
 
 @pytest.fixture(scope="session")
 def note_fixture():
