845e3b3c78fce5e1e6bf6e7a5f48b9c0a1e8ccc0
Clément Schreiner <clement@mux.me>
2020-08-25 23:02:15 +0200

    add pocket integration test

---
+++ tests/integration/test_pocket.py
@@ -0,0 +1,26 @@
+import responses
+from flask import Flask
+
+from archivy import data
+from archivy.extensions import get_db
+
+
+def test_parse_pocket(test_app, client, mocked_responses, pocket_fixture):
+    """Test the /pocket endpoint
+
+    HTTP calls to the pocket API are mocked out
+    """
+
+    # fake website
+    mocked_responses.add(responses.GET, "https://example.com/", body="""<html>
+        <head><title>Example</title></head><body><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </p></body></html>
+    """)
+
+
+    r: Flask.response_class = client.get('/parse_pocket?new=1')
+    assert r.status_code == 302
+
+    dataobjs = data.get_items()
+    assert len(dataobjs.child_files) == 1
