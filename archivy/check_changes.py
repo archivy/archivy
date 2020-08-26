ceb32eb0c78a3502b5813984016a45d3c4f15ad4
Clément Schreiner <clement@mux.me>
2020-08-27 01:42:10 +0200

    use app.logger instead of print for logs
     - some refactoring in the search functionality was needed to run everything in
     an app context
     - logging level set to INFO at app instantiation

--- archivy/check_changes.py
+++ archivy/check_changes.py
@@ -1,37 +1,44 @@
+import os
 import time
 
+import flask
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 from archivy import search, models
-from archivy.config import Config
 
 
 class ModifHandler(FileSystemEventHandler):
+    def __init__(self, app: flask.Flask):
+        self.app = app
 
     def on_modified(self, event):
-        if event.src_path.endswith(
-                ".md"):
-            print(f"Changes to {event.src_path}")
-            dataobj = models.DataObj.from_file(event.src_path)
-            search.add_to_index(Config.INDEX_NAME, dataobj)
-            self.last_modify_event = event.src_path
+        with self.app.app_context():
+            if event.src_path.endswith(
+                    ".md"):
+                self.app.logger.info(f"Detected changes to {event.src_path}")
+                dataobj = models.DataObj.from_file(event.src_path)
+                search.add_to_index(self.app.config['INDEX_NAME'], dataobj)
+                self.last_modify_event = event.src_path
 
     def on_deleted(self, event):
-        if event.src_path.endswith(".md"):
-            id = event.src_path.split("/")[-1].split("-")[0]
-            search.remove_from_index(Config.INDEX_NAME, id)
-            print(f"{event.src_path} has been removed")
-            self.last_delete_event = event.src_path
+        with self.app.app_context():
+            if event.src_path.endswith(".md"):
+                id = event.src_path.split("/")[-1].split("-")[0]
+                search.remove_from_index(self.app.config['INDEX_NAME'], id)
+                self.app.logger.info(f"{event.src_path} has been removed")
+                self.last_delete_event = event.src_path
 
 
-def run_watcher():
-    event_handler = ModifHandler()
+def run_watcher(app):
+    event_handler = ModifHandler(app)
     observer = Observer()
     observer.schedule(
         event_handler,
-        path=Config.APP_PATH +
-        "/data/",
+        path=os.path.join(
+            app.config['APP_PATH'],
+            'data/'
+        ),
         recursive=True)
     observer.start()
 
