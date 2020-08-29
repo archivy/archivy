64b6f61da2f0d8c19709a13b6f5c017f544a4bee
Clément Schreiner <clement@mux.me>
2020-08-30 01:27:35 +0200

    test add_bookmark with missing fields

--- tests/integration/test_views.py
+++ tests/integration/test_views.py
@@ -1,4 +1,7 @@
 from flask.testing import FlaskClient
+from responses import RequestsMock, GET
+
+from archivy.extensions import get_max_id
 
 
 def test_get_index(test_app, client: FlaskClient):
@@ -10,6 +13,15 @@ def test_get_new_bookmark(test_app, client: FlaskClient):
     response = client.get('/bookmarks/new')
     assert response.status_code == 200
 
+
+def test_post_new_bookmark_missing_fields(test_app, client: FlaskClient,
+                           mocked_responses: RequestsMock):
+
+    response = client.post('/bookmarks/new', data={
+        'submit': True
+    })
+    assert response.status_code == 200
+    assert b'This field is required' in response.data
 
 def test_get_new_note(test_app, client: FlaskClient):
     response = client.get('/notes/new')
