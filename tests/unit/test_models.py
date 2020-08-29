b1d6b02dbf55d8beb0cb071ed27ccdf6e26a167c
Uzay-G <uzgirit@gmail.com>
2020-08-29 12:33:03 +0200

    tests for bookmarks

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,5 @@
 import frontmatter
+import responses
 
 from archivy.extensions import get_max_id
 
@@ -10,6 +11,7 @@ def test_new_note(test_app, note_fixture):
     Check that a new note is correctly saved into the filesystem
     with the right attributes and the right id.
     """
+
     with test_app.app_context():
         max_id = get_max_id()
         assert note_fixture.id == max_id
@@ -17,3 +19,25 @@ def test_new_note(test_app, note_fixture):
     saved_file = frontmatter.load(note_fixture.fullpath)
     for attr in attributes:
         assert getattr(note_fixture, attr) == saved_file[attr]
+
+def test_bookmark_sanitization(test_app, client, mocked_responses, bookmark_fixture):
+    """Tests bookmark contents are correctly saved and converted to proper Markdown"""
+
+    with test_app.app_context():
+        assert bookmark_fixture.id == get_max_id()
+
+    saved_file = frontmatter.load(bookmark_fixture.fullpath)
+    for attr in attributes:
+        assert getattr(bookmark_fixture, attr) == saved_file[attr]
+    assert bookmark_fixture.url == saved_file["url"] 
+
+    # remove buggy newlines that interfere with checks:
+    bookmark_fixture.content = bookmark_fixture.content.replace("\n", "")
+    # test script is sanitized 
+    assert bookmark_fixture.content.find("<script>") == -1
+    # test relative urls in the HTML are remapped to an absolute urls
+    assert bookmark_fixture.content.find("example.com/images/image1.png") != -1
+    assert bookmark_fixture.content.find("example.com/testing-absolute-url") != -1
+    
+
+
