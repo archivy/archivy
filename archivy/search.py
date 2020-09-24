03700ca614392570ff457d4dd950c9ee8f349b56
Uzay-G <uzgirit@gmail.com>
2020-09-24 21:42:53 +0200

    ADD: use builtin Elasticsearch highlighting for better quality of res… (#80)
    * ADD: use builtin Elasticsearch highlighting for better quality of results
    
    * significant improvements to speed by joining highlight chunks

--- archivy/search.py
+++ archivy/search.py
@@ -1,3 +1,5 @@
+from pypandoc import convert_text
+
 from archivy.extensions import get_elastic_client
 
 
@@ -29,7 +31,38 @@ def query_index(index, query):
                 "multi_match": {
                     "query": query,
                     "fields": ["*"],
-                    "analyzer": "rebuilt_standard"}}},
+                    "analyzer": "rebuilt_standard"
+                }
+            },
+            "highlight": {
+                "fields": {
+                    "content": {
+                        "pre_tags": "<span style='background-color: #f6efa6'>",
+                        "post_tags": "</span>",
+                        "boundary_max_scan": 200,
+                        "fragment_size": 0
+                    }
+                }
+            }
+        }
     )
 
-    return search["hits"]["hits"]
+    hits = []
+    for hit in search["hits"]["hits"]:
+        formatted_hit = {"id": hit["_id"], "title": hit["_source"]["title"], "highlight": []}
+        if "highlight" in hit:
+            # FIXME: find a way to make this less hacky and
+            # yet still conserve logical separations
+            # hack to make pandoc faster by converting highlights in one go
+            # join highlights into string with symbolic separator
+            SEPARATOR = "~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~"
+            concatenated_highlight = SEPARATOR.join(
+                    [highlight for highlight in hit["highlight"]["content"]])
+            # re split highlights
+            formatted_hit["highlight"] = convert_text(concatenated_highlight,
+                                                      "html",
+                                                      format="md").split(SEPARATOR)
+
+        hits.append(formatted_hit)
+
+    return hits
