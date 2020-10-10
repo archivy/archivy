95e2d8fbd43e3c1fdc363a9ab37e0d72c5cbcbad
Uzay-G <uzgirit@gmail.com>
2020-10-10 09:59:54 +0200

    REWRITE: use pathlib instead of os for better compatibility and better code. (#103)
    * REWRITE: use pathlib instead of os for better compatibility and better code
    
    * REMOVE: unnecessary prints and fix bug
    
    * minor fixes
    
    * FIX: small bug with previous commits

--- archivy/__init__.py
+++ archivy/__init__.py
@@ -33,10 +33,9 @@ if app.config["ELASTICSEARCH_ENABLED"]:
     with app.app_context():
         es = extensions.get_elastic_client()
         try:
-            print(
-                es.indices.create(
-                    index=app.config["INDEX_NAME"],
-                    body=app.config["ELASTIC_CONF"]))
+            es.indices.create(
+                index=app.config["INDEX_NAME"],
+                body=app.config["ELASTIC_CONF"])
         except elasticsearch.ElasticsearchException:
             app.logger.info("Elasticsearch index already created")
 
