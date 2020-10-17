58647b91bbbcac773ff0174820ccca23d5ee3754
Uzay-G <uzgirit@gmail.com>
2020-10-17 19:47:12 +0200

    v0.6.0
    New major features:
    - login system
    - dark themes
    
    This login system which will be developed for more of a multi-user
    approach with well-defined permissions in further releases now allows
    you to self-host archivy on a server and access it from anywhere without
    worrying about your data being deleted / modified.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setuptools.setup(
     name="archivy",
-    version="0.5.0",
+    version="0.6.0",
     author="Uzay-G",
     author_email="uzgirit@gmail.com",
     description=(
