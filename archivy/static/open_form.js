67b6e82d7b91c1a6a6087d0768809adf6f1861c9
Uzay-G <uzgirit@gmail.com>
2020-11-07 14:40:40 +0100

    ADD: basic web ui for plugins

---
+++ archivy/static/open_form.js
@@ -0,0 +1,36 @@
+function openCommand(cmdUrl, updateState, menuItem) {
+    if (REQUEST_RUNNING) {
+        let leave = confirm("Command is still running. Leave anyway?");
+        if (leave) {
+            // hack as this is global on page and if we leave the other command cannot run.
+            // TODO: fetch will continue to run.
+            //  Do a real abort of fetch: https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
+            REQUEST_RUNNING = false;
+        } else {
+            return;
+        }
+    }
+
+    let formDiv = document.getElementById('form-div');
+    // if (updateState) {
+    //
+    //     history.pushState({'cmdUrl': cmdUrl}, null, cmdUrl);
+    // }
+    fetch(cmdUrl)
+        .then(function(response) {
+            selectMenuItem(menuItem);
+            return response.text();
+        })
+        .then(function(theFormHtml) {
+            formDiv.innerHTML = theFormHtml;
+        });
+}
+
+function selectMenuItem(menuItem) {
+    var x = document.getElementsByClassName("command-selected");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('command-selected');
+    }
+    menuItem.classList.add('command-selected');
+}
