6f9472b77b181454b5ad975bc511a0b8d47606b5
Uzay-G <uzgirit@gmail.com>
2020-11-17 17:57:57 +0100

    Add link to plugins on homepage

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setuptools.setup(
     name="archivy",
-    version="0.8.2",
+    version="0.8.3",
     author="Uzay-G",
     author_email="uzgirit@gmail.com",
     description=(
