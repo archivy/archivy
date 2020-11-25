fc8209fa367e66ca003a14820424f563e9bb7431
Uzay-G <uzgirit@gmail.com>
2020-11-25 09:36:20 +0100

    remove useless line

--- conftest.py
+++ conftest.py
@@ -26,7 +26,6 @@ def test_app():
         _app = create_click_web_app(cli, cli.cli, app) 
     app_dir = tempfile.mkdtemp()
     _app.config['APP_PATH'] = app_dir
-    os.environ["APP_PATH"] = app_dir
     data_dir = os.path.join(app_dir, "data")
     os.mkdir(data_dir)
 
