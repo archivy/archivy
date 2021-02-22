2f7581b626cb2fb33e77694b9ed94ed70622d13a
Uzay-G <uzgirit@gmail.com>
2021-02-22 15:05:45 +0100

    v1.0.2
    Fixes and improvements to backend with accessibility improvements.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setuptools.setup(
     name="archivy",
-    version="1.0.1",
+    version="1.0.2",
     author="Uzay-G",
     author_email="uzgirit@gmail.com",
     description=(
