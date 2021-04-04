8a4798cca3d78c76b5b78ebba6299d71d58ad992
Uzay-G <uzgirit@gmail.com>
2021-04-04 19:17:47 +0200

    v1.1.3rc1 to test backlink changes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setuptools.setup(
     name="archivy",
-    version="1.1.2",
+    version="1.1.3rc1",
     author="Uzay-G",
     author_email="uzgirit@gmail.com",
     description=(
