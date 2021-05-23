5ab7ade9c1e2afc48620ca4bf0c0db56c6f9280d
Uzay-G <uzgirit@gmail.com>
2021-05-23 14:19:02 +0200

    v1.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setuptools.setup(
     name="archivy",
-    version="1.2.0",
+    version="1.3.0",
     author="Uzay-G",
     author_email="uzgirit@gmail.com",
     description=(
