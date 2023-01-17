ffadbea84c7f6ad6dab1bfa7ef5c8147bc6f4c5e
Uzay-G <uzgirit@gmail.com>
2023-01-17 16:58:20 +0100

    v1.7.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setuptools.setup(
     name="archivy",
-    version="1.7.3",
+    version="1.7.4",
     author="Uzay-G",
     author_email="halcyon@disroot.org",
     description=(
